38bab6a99e61437d29c4faaab574022c3e51bee2
xinghe <ellen.gy@antgroup.com>
2024-12-10 19:33:47 +0800

    提交新的靶场

--- src/main/java/com/alipay/Main.java
+++ src/main/java/com/alipay/Main.java
@@ -1,7 +1,12 @@
 package com.alipay;
 
+import cloud.agileframework.spring.util.ParamUtil;
+
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ParamUtil.getInParamOfFiles(new HashMap<>(), "");
     }
 }
