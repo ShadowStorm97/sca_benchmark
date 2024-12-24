ef9eda8efa8352383200c4cd21d0aeb5cabd422b
xinghe <ellen.gy@antgroup.com>
2024-12-24 23:00:15 +0800

    fux

--- src/main/java/com/alipay/Main.java
+++ src/main/java/com/alipay/Main.java
@@ -7,6 +7,6 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        ParamUtil.getInParam(new HashMap<>(), "", Object.class);
+        //ParamUtil.getInParam(new HashMap<>(), "", Object.class);
     }
 }
